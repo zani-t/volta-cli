65752746325e048c57f2112647c3d6457307666f
zani-t <tembo4.t@gmail.com>
2023-01-22 19:20:44 -0500

    random forest initial support

--- vcx/ml_utils/trainer.py
+++ vcx/ml_utils/trainer.py
@@ -1,6 +1,7 @@
 # Model object initializer & trainer
 
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
@@ -48,7 +49,7 @@ def train(
     # Initialize and train model
     arch = mysql_model[5]
 
-    if (arch == "LogisticRegression") or (arch == "LogReg"):
+    if arch == "LogisticRegression" or arch == "LogReg":
         if not label:
             # ADD NO LABEL ERROR
             raise Exception
@@ -58,7 +59,7 @@ def train(
         clf = LogisticRegression(penalty=penalty, random_state=rs_m, max_iter=max_iter).fit(X_train, y_train)
         print(clf.score(X_test, y_test))
     
-    if (arch == "SupportVectorMachine") or (arch == "SVM"):
+    if arch == "SupportVectorMachine" or arch == "SVM":
         if not label:
             # ADD NO LABEL ERROR
             raise Exception
@@ -67,6 +68,20 @@ def train(
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=rs_ds)
         clf = SVC(kernel=kernel, random_state=rs_m).fit(X_train, y_train) # ADD REGULARIZATION C
         print(clf.score(X_test, y_test))
+    
+    if arch == "RandomForest" or arch == "RF":
+        if not label:
+            # ADD NO LABEL ERROR
+            raise Exception
+        y = data[label]
+        X = data.drop(label, axis=1)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=rs_ds)
+        """ clf = RandomForestClassifier(
+            n_estimators=n_estimators,
+            max_depth=max_depth,
+            min_samples_split=min_samples_split,
+            random_state=rs_m,
+            n_jobs=n_jobs) """
 
     # Display eval metric and prompt user to save
 
